d655652d604a137d3f766597a976c984ca0eee9b
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2023-08-11 15:29:49 +0300

    update Projects.jsx fn sort and filter

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as Group } from "../../images/icons/people team.svg";
 import s from "./projects.module.scss";
 import Filter from "../Filter/Filter";
 const Projects = () => {
-  const [projectData, setProjectData] = useState(null);
+  const [projectData, setProjectData] = useState([]);
   const [filter, setFilter] = useState("all");
 
   useEffect(() => {
@@ -19,67 +19,74 @@ const Projects = () => {
   const handleFilterChange = (filter) => {
     setFilter(filter);
   };
+  const sortedProjects = projectData
+    .map((obj) => {
+      return { ...obj, date: new Date(obj.date) };
+    })
+    .sort((a, b) => Number(b.date) - Number(a.date));
+
   const filterProjects = (projects, filter) => {
     return projects.filter((project) => {
       if (filter === "all") {
         return true;
-      } else if (filter === "solo" && project.type === "solo") {
+      } else if (filter === "solo" && project.projectType === "solo") {
         return true;
-      } else if (filter === "group" && project.type === "group") {
+      } else if (filter === "group" && project.projectType === "group") {
         return true;
       }
       return false;
     });
   };
-  const filteredProjects = filterProjects(projectData, filter);
-  
+
+  const filteredProjects =
+    projectData && filterProjects(sortedProjects, filter);
+
   return (
     <section className={s.section_projects}>
       <div className={`container ${s.projects}`}>
         <h1 className="title_page">Projects</h1>
         <Filter onChangeFilter={handleFilterChange} />
-        {projectData &&
-          filteredProjects.map((project) => (
-            <div className={s.block} key={project._id}>
-              <div className={s.block_title}>
-                <h2 className={s.title}>{project.title}</h2>
-                {project.projectType === "group" ? (
-                  <Group className={s.block_icon} />
-                ) : (
-                  <Solo className={s.block_icon} />
-                )}
-              </div>
-              <h3 className={s.library}>{project.library}</h3>
-              <p className={s.description}>{project.description}</p>
-              <img
-                src={urlFor(project.image).url()}
-                alt={project.image.alt}
-                className={s.picture}
-              />
-              <ul className={s.wrapBtn}>
-                <li className={`${s.btn} ${s.btn_view}`}>
-                  <a
-                    className={s.linkView}
-                    href={project.liveLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    view
-                  </a>
-                </li>
-                <li className={`${s.btn} ${s.btn_github}`}>
-                  <a
-                    className={s.linkGitHub}
-                    href={project.codeLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    github
-                  </a>
-                </li>
-              </ul>
+        {filteredProjects.map((project) => (
+          <div className={s.block} key={project._id}>
+            <div className={s.block_title}>
+              <h2 className={s.title}>{project.title}</h2>
+              {project.projectType === "group" ? (
+                <Group className={s.block_icon} />
+              ) : (
+                <Solo className={s.block_icon} />
+              )}
             </div>
-          ))}
+            <h3 className={s.library}>{project.library}</h3>
+            <p className={s.description}>{project.description}</p>
+            <img
+              src={urlFor(project.image).url()}
+              alt={project.image.alt}
+              className={s.picture}
+            />
+            <ul className={s.wrapBtn}>
+              <li className={`${s.btn} ${s.btn_view}`}>
+                <a
+                  className={s.linkView}
+                  href={project.liveLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  view
+                </a>
+              </li>
+              <li className={`${s.btn} ${s.btn_github}`}>
+                <a
+                  className={s.linkGitHub}
+                  href={project.codeLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  github
+                </a>
+              </li>
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
